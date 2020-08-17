c036b76dac84ba5fe4feb1e19e86a04fc204d1f7
takumi.naitou <hello7110baseball7@icloud.com>
2020-08-17 23:21:44 +0000

    CRUD task 修正版

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -41,7 +41,7 @@ class TasksController < ApplicationController
   
   def destroy
     @task = Task.find(params[:id])
-    @task = destroy
+    @task.destroy
     
     flash[:success] = "Task は正常に削除されました"
     redirect_to tasks_url
