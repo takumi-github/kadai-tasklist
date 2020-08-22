4e00b810f24aeb4205a491040e7f63c31ac99863
takumi.naitou <hello7110baseball7@icloud.com>
2020-08-22 04:53:53 +0000

    user login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base 
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end  
 end
