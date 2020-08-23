77ecdf283f11fdcccf2811182e60e3f2b68a7b6c
takumi.naitou <hello7110baseball7@icloud.com>
2020-08-23 06:23:16 +0000

    user 紐づけ

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,31 +1,37 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :update, :destroy]
   before_action :require_user_logged_in
   
   def index
-    @tasks = Task.all.page(params[:page]).per(3)
+    if logged_in?
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(3)
+    end
   end
   
   def show
   end
   
   def new
-    @task = Task.new
+    if logged_in?
+    @task = current_user.tasks.build
+    end
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = "Taskが正常に投稿されました"
       redirect_to @task
     else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
       flash.now[:danger] = "Task が投稿されませんでした"
       render :new
     end  
   end
   
   def edit
+      @task = current_user.tasks.build
   end
   
   def update
